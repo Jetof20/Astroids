c510888b2994c34d0670b60c895273b896fcb25f
Jens Christian Toftdahl <jetof20@student.sdu.dk>
2022-02-09 10:47:01 +0100

    lecture 2

--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -4,7 +4,17 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IGamePluginService {
+    /**
+     * this method will be called when the game starts, put code here reliant on startup
+     * @param gameData The gamedata
+     * @param world The worlddata
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * this method is called when the game is stopped
+     * @param gameData The gamedata
+     * @param world The worlddata
+     */
     void stop(GameData gameData, World world);
 }
